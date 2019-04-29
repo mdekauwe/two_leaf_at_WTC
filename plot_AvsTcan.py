a58bded101bd278e197f9ecdaab7611df0cefc6a
mdekauwe <mdekauwe@gmail.com>
2019-04-30 09:19:25 +1000

    Added plotting script

---
+++ plot_AvsTcan.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Plot the A as a function of Tcan
+
+"""
+import os
+import sys
+import numpy as np
+import math
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+__author__  = "Martin De Kauwe"
+__version__ = "1.0 (30.04.2019)"
+__email__   = "mdekauwe@gmail.com"
+
+
+output_dir = "outputs"
+
+fig = plt.figure(figsize=(14,9))
+fig.subplots_adjust(hspace=0.1)
+fig.subplots_adjust(wspace=0.05)
+plt.rcParams['text.usetex'] = False
+plt.rcParams['font.family'] = "sans-serif"
+plt.rcParams['font.sans-serif'] = "Helvetica"
+plt.rcParams['axes.labelsize'] = 14
+plt.rcParams['font.size'] = 14
+plt.rcParams['legend.fontsize'] = 14
+plt.rcParams['xtick.labelsize'] = 14
+plt.rcParams['ytick.labelsize'] = 14
+
+chambers = ["C01", "C02", "C03", "C04", "C05", "C06", \
+            "C07", "C08", "C09", "C10", "C11", "C12"]
+for i,chamber in enumerate(chambers):
+
+
+    fn = os.path.join(output_dir, "wtc_two_leaf_%s.csv" % (chamber))
+    df_2l = pd.read_csv(fn)
+
+    fn = os.path.join(output_dir, "wtc_big_leaf_%s.csv" % (chamber))
+    df_bl = pd.read_csv(fn)
+
+    ax = fig.add_subplot(4,3,1+i)
+
+    ax.plot(df_2l.T_can, df_2l.An_can, ".", label="2-leaf")
+    ax.plot(df_bl.T_can, df_bl.An_can, ".", label="Big leaf")
+
+
+    if i == 10:
+        ax.set_xlabel("T$_{canopy}$ ($^{\circ}\mathrm{C}$)")
+
+    if i == 0:
+        ax.legend(numpoints=1, loc="best")
+
+    if i < 9:
+        plt.setp(ax.get_xticklabels(), visible=False)
+
+    if i != 0 and i != 3 and i != 6 and i != 9:
+        plt.setp(ax.get_yticklabels(), visible=False)
+
+    if i == 6:
+        ax.set_ylabel("An (\u03BCmol m$^{-2}$s$^{-1}$)", position=(2.5, 1.2))
+    
+plt.show()
